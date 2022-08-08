107414882a4f59bb69519e24a0b0d9c6b215836d
Stefan Dojcinovic <stefan.dojcinovic051@gmail.com>
2022-08-08 20:07:14 +0200

    Initial Commit

---
+++ MessangerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessangerApp
+//
+//  Created by Stefan Dojcinovic on 8.8.22..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
